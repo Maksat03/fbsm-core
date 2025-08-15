47da3b276315df995d71f4c4470294e896f24c0b
ainzzzer <ussin.batyr@gmail.com>
2025-08-15 17:26:39 +0500

    UsernameField

--- core/consts.py
+++ core/consts.py
@@ -1,2 +1,6 @@
+PHONE_REGEX = r"^\+\d{1,4}\s\d{3}\s\d{3}\s\d{4}$"
+
 # Список доступных country-codes по которым можно логинится
-COUNTRY_CODES = ("+7",)
+COUNTRY_CODES = {
+    "+7",
+}
--- core/fields.py
+++ core/fields.py
@@ -1,28 +1,64 @@
-import re
-
+from django.core.exceptions import ValidationError as DjangoValidationError
+from django.core.validators import EmailValidator
 from rest_framework import serializers
 from rest_framework.fields import RegexValidator
 
-from core.consts import COUNTRY_CODES
-
-codes_pattern = "|".join(re.escape(code) for code in COUNTRY_CODES)
-
-PHONE_REGEX = rf"^({codes_pattern})\s\d{{3}}\s\d{{3}}\s\d{{4}}$"
-
-pattern = re.compile(PHONE_REGEX)
+from core.consts import COUNTRY_CODES, PHONE_REGEX
 
 
 class PhoneNumberField(serializers.CharField):
     default_error_messages = {
-        "invalid_format": "Неверный формат.",
+        "invalid": "Введите корректный номер телефона.",
+        "invalid_country_code": "Данный регион не поддерживается.",
     }
 
     def __init__(self, **kwargs):
         kwargs["min_length"] = 15
-        kwargs["max_length"] = 17
+        kwargs["max_length"] = 18
         kwargs["validators"] = [
             RegexValidator(
                 regex=PHONE_REGEX, message=self.default_error_messages["invalid_format"]
             )
         ]
         super().__init__(**kwargs)
+
+
+class UsernameField(serializers.CharField):
+    default_error_messages = {
+        "invalid": "Введите корректный телефон или email.",
+        "invalid_country_code": "Данный регион не поддерживается.",
+    }
+
+    def __init__(self, **kwargs):
+        kwargs.setdefault("min_length", 5)
+        kwargs.setdefault("max_length", 254)
+        super().__init__(**kwargs)
+        self.phone_validator = RegexValidator(
+            regex=PHONE_REGEX, message=self.default_error_messages["invalid"]
+        )
+        self.email_validator = EmailValidator(
+            message=self.default_error_messages["invalid"]
+        )
+
+    def run_validation(self, data):
+        value = super().run_validation(data)
+
+        # Если это email — сразу возвращаем
+        try:
+            self.email_validator(value)
+            return value
+        except DjangoValidationError:
+            pass
+
+        # Проверяем формат телефона
+        try:
+            self.phone_validator(value)
+        except DjangoValidationError:
+            self.fail("invalid")
+
+        # Проверяем код страны отдельно
+        country_code = value.split(" ")[0]
+        if country_code not in COUNTRY_CODES:
+            self.fail("invalid_country_code")
+
+        return value
