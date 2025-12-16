586a6fa74f0c154cccd7f52ad5172a2e29378e10
ainzzzer <ussin.batyr@gmail.com>
2025-12-16 15:24:50 +0500

    fix(exception_handler): extra

--- core/exceptions.py
+++ core/exceptions.py
@@ -2,12 +2,14 @@ import logging
 
 from django.db.models import ProtectedError
 from django.utils.deprecation import MiddlewareMixin
-
-from rest_framework.views import exception_handler
+from rest_framework.exceptions import (
+    APIException,
+    AuthenticationFailed,
+    NotAuthenticated,
+)
 from rest_framework.response import Response
-from rest_framework.exceptions import APIException, NotAuthenticated, AuthenticationFailed
-from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
-
+from rest_framework.views import exception_handler
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 
 logger = logging.getLogger(__name__)
 EXCEPTIONS = {
@@ -16,49 +18,55 @@ EXCEPTIONS = {
             "detail": "Токен не предоставлен",
             "code": "not_authenticated",
         },
-        "status": 401
+        "status": 401,
     },
     AuthenticationFailed: {
         "response": {
             "detail": "Неверные учетные данные",
             "code": "authentication_failed",
         },
-        "status": 401
+        "status": 401,
     },
     TokenError: {
         "response": {
             "detail": "Ошибка токена",
             "code": "token_error",
         },
-        "status": 401
+        "status": 401,
     },
     InvalidToken: {
         "response": {
             "detail": "Недействительный токен",
             "code": "invalid_token",
         },
-        "status": 401
+        "status": 401,
     },
     ProtectedError: {
         "response": {
             "detail": "Защищено от удаление",
             "code": "protected_from_deletion",
         },
-        "status": 403
+        "status": 403,
     },
     500: {
         "response": {
             "detail": "Внутренняя ошибка",
             "code": "internal_error",
         },
-        "status": 500
-    }
+        "status": 500,
+    },
 }
 
 
 def drf_exc_handler(exc, context):
-    user_id = context["request"].user.get("user_id") if isinstance(context["request"].user, dict) else None
-    logger.exception(f"[DRF Exc Handler] Ошибка: {exc} | User: {user_id}", exc_info=True)
+    user_id = (
+        context["request"].user.get("user_id")
+        if isinstance(context["request"].user, dict)
+        else None
+    )
+    logger.exception(
+        f"[DRF Exc Handler] Ошибка: {exc} | User: {user_id}", exc_info=True
+    )
 
     if response := exception_handler(exc, context):
         exception = EXCEPTIONS.get(type(exc), None)
@@ -66,7 +74,12 @@ def drf_exc_handler(exc, context):
         if exception:
             return Response(exception["response"], status=exception["status"])
 
-        response.data["code"] = response.data.get("code", getattr(exc, "default_code", response.status_code))
+        response.data["code"] = response.data.get(
+            "code", getattr(exc, "default_code", response.status_code)
+        )
+        extra = getattr(exc, "extra", None)
+        if extra:
+            response.data["extra"] = extra
         return response
     else:
         exception = EXCEPTIONS.get(type(exc), EXCEPTIONS[500])
@@ -75,8 +88,12 @@ def drf_exc_handler(exc, context):
 
 class DjExcHandlerMiddleware(MiddlewareMixin):
     def process_exception(self, request, exc):
-        user_id = request.user.get("user_id") if isinstance(request.user, dict) else None
-        logger.exception(f"[DJ Exc Handler] Ошибка: {exc} | User: {user_id}", exc_info=True)
+        user_id = (
+            request.user.get("user_id") if isinstance(request.user, dict) else None
+        )
+        logger.exception(
+            f"[DJ Exc Handler] Ошибка: {exc} | User: {user_id}", exc_info=True
+        )
         return Response(EXCEPTIONS[500]["response"], status=500)
 
 
